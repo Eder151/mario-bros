d784e8c39e998e949d43cc40f414ffab5f4fa39d
Eder151 <a23310305@ceti.mx>
2024-10-16 00:14:11 +0000

    clase 2

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <Porcentaje.hpp>
+
+int main(){
+    Porcentaje foco;
+    Porcentaje foco1;
+    Porcentaje foco2;
+    Porcentaje foco3;
+
+
+    foco1.Incrementar(10);
+    foco1.Incrementar(10);
+    foco1.Disminuir(4);
+
+
+    std::cout
+    <<"Foco 1: "
+    <<foco1.Inprimir()
+    <<std::endl;
+
+}
