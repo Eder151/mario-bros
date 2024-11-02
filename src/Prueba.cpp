47143d6c7a4a85fc8e942444f6ab236399ed73bf
Eder151 <a23310305@ceti.mx>
2024-11-02 15:12:00 +0000

    Examen

--- src/Prueba.cpp
+++ src/Prueba.cpp
@@ -8,27 +8,53 @@
 using namespace std;
 using namespace ftxui;
 
+int posicionX(int posicion, int velocidad, int tiempo)
+{
+    return posicion + velocidad * tiempo;
+}
+
 int main(int argc, char const *argv[])
 {
     std::string reset_position;
     int frame = 0;
-    auto can = Canvas(300, 300);
-    can.DrawPointCircle(30, 30, 30);
-    can.DrawPointLine(10, 50, 50, 10);
-    can.DrawPointLine(50,50,0,0);
-    can.DrawPointCircle(20, 20, 20);
-    auto ColorSupport = vbox({
+    int vix = 5;
+    int posx = 100;
+    int posy = 100;
 
-    });
+    const int canvas_width = 300;
+    const int canvas_height = 300;
+
     while (true)
     {
         Screen pantalla = Screen::Create(Dimension::Full(), Dimension::Full());
-        Element lienzo = bgcolor(Color::DarkViolet, border(vbox(ColorSupport, canvas(&can))));
+
+        int nueva_posx = posicionX(posx, vix, frame);
+
+        auto can = Canvas(canvas_width, canvas_height);
+
+        can.DrawPointCircle(nueva_posx % canvas_width, posy + 10, 10, Color::GrayDark);       
+        can.DrawPointCircle((nueva_posx + 60) % canvas_width, posy + 10, 10, Color::GrayDark); 
+
+        for (int i = nueva_posx % canvas_width; i < (nueva_posx + 60) % canvas_width; ++i)
+        {
+            can.DrawPoint(i, posy, Color::Blue); 
+        }
+        can.DrawPoint((nueva_posx + 30) % canvas_width, posy - 20, Color::Blue); 
+        can.DrawPoint((nueva_posx + 30) % canvas_width, posy - 30, Color::Blue); 
+
+        can.DrawPointLine(nueva_posx % canvas_width, posy + 10, (nueva_posx + 30) % canvas_width, posy, Color::Blue);    
+        can.DrawPointLine((nueva_posx + 60) % canvas_width, posy + 10, (nueva_posx + 30) % canvas_width, posy, Color::Blue); 
+
+        Element lienzo = bgcolor(Color::Black, canvas(&can));
         Render(pantalla, lienzo);
-        std::cout << reset_position;
+
         pantalla.Print();
         reset_position = pantalla.ResetPosition(true);
-        this_thread::sleep_for(0.1s);
+
+        std::cout << "Frame: " << frame << ", X: " << nueva_posx << std::endl;
+
+        this_thread::sleep_for(0.5s);
+
         frame++;
     }
 
